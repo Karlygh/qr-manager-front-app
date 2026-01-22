f1af29a6806d78d2576a89c734dcb3cbf212452e
Carlos Grandio Hidalgo <carlosgrandio9@gmail.com>
2026-01-22 17:36:35 +0100

    Integración de nuevos componentes para carta principal. -Donde futuramente veran desde la carta , horarios,wifi,instalaciones,redes en el navbar de carta-principal.

---
+++ src/app/pages/info-clientes/info-clientes.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-info-clientes',
+  standalone:true,
+  imports: [],
+  templateUrl: './info-clientes.html',
+  styleUrl: './info-clientes.css'
+})
+export class InfoClientes {
+
+}
---
+++ src/app/pages/info-clientes/wifi-clientes/wifi-clientes.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wifi-clientes',
+  standalone:true,
+  imports: [],
+  templateUrl: './wifi-clientes.html',
+  styleUrl: './wifi-clientes.css'
+})
+export class WifiClientes {
+
+}
